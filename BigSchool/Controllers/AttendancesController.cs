2b440f5a80c643d777c3fdf224c5207a24bc5db7
MYLINHDOAN <doanmxlinh@gmail.com>
2021-07-07 17:08:53 +0700

    Khoi tao

---
+++ BigSchool/Controllers/AttendancesController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using BigSchool.Models;
+using System.Web.Http;
+
+namespace BigSchool.Controllers
+{
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext _dbContext;
+        public AttendancesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        [System.Web.Http.HttpPost]
+        public IHttpActionResult Attend ([FromBody] int courseId)
+        {
+            var attendance = new Attendance
+            {
+                CourseId = courseId,
+                AttendeeId = User.Identity.GetUserId()
+            };
+            _dbContext.Attendances.Add(attendance);
+            _dbContext.SaveChanges();
+            return Ok();
+        }
+
+    }
+}
---
+++ SQLQuery1.sql
@@ -0,0 +1,2 @@
+select *from BigSchool
+SELECT *FROM CATEGORIES
